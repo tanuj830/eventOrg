2c366cb4a4504fb177aca515c7379edefa1934e7
tanuj <tanujbhatt644@gmail.com>
2023-07-28 17:20:26 +0530

    worked on hero section...!

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,14 +4,14 @@ const Hero = () => {
   return (
 <div className=' h-[90vh] md:px-24  lg:px-40 lg:mt-10 xl:p-30 sm:px-10 text-white bg-[url(https://cdn.thecodehelp.in/m64udmyhucj2zb42uvyf_06640ff501.svg)] bg-no-repeat bg-left-bottom   '>
 <div className='flex items-center justify-start xl:justify-center w-full gap- xl:gap-40'>
-<div className=' mt-24 md:mt-30 lg:mt-0 px-5 lg:w-[50vw] flex items-center justify-end'>
+<div className=' mt-20 sm:mt-24 md:mt-30 lg:mt-0 px-5 lg:w-[50vw] flex items-center justify-end'>
     <div className=''>
     <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-e0b16.appspot.com/o/ticket.gif?alt=media&token=ea2cf2f6-8ac7-4c36-a7c8-40446c2c6422" className='xl:w-36 md:w-24 w-20 rounded-full transition-all shadow-2xl shadow-gray-800' alt="" />
 
 <h1 className='xl:text-[100px] mt-3 md:mt-10 lg:mt-0 lg:text-[80px] text-[60px] font-bold '>Collegify.</h1>
 <p className='hidden lg:inline-block text-base mt-10 xl:mt-10 '>Welcome to Book College Events, your one-stop-shop for <br /> affordable and accessible event organizing and booking. Say <br /> goodbye to traditional difficulties and hello to seamless payment <br /> gateways and magnificent event management.
 </p>
-<p className='inline-block lg:hidden text-base mt-10 xl:mt-10 font-medium'>Welcome to Book College Events, your one-stop-shop for affordable and accessible event organizing and booking. Say  goodbye to traditional difficulties and hello to seamless payment  gateways and magnificent event management.
+<p className='inline-block lg:hidden text-md mt-10 xl:mt-10 font-medium'>Welcome to Book College Events, your one-stop-shop for affordable and accessible event organizing and booking. Say  goodbye to traditional difficulties and hello to seamless payment  gateways and magnificent event management.
 
 </p>
 <div className='mt-8 md:mt-10 flex justify-start items-center text-black gap-1 md:gap-4'>
